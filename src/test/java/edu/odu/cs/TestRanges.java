0f501f00fbb6209ae5c7f8a88c5db3b0c70f64dd
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2022-04-28 21:44:08 -0500

    Commit Inicial

---
+++ src/test/java/edu/odu/cs/TestRanges.java
@@ -0,0 +1,27 @@
+package edu.odu.cs;
+
+import edu.odu.cs.Interval;
+import edu.odu.cs.Ranges;
+import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+/**
+ * Test of the Ranges class
+ */
+public class TestRanges {
+
+    double precision = 0.001;
+
+    @Test
+    public void testConstructor() {
+        Ranges ranges = new Ranges(1.0, 100.0);
+        assertThat(ranges.sum(), closeTo(99.0, precision));
+
+        Interval[] expected = { new Interval(1.0, 100.0) };
+        assertThat (ranges, contains(expected)); // checks ranges.iterator()
+    }
+
+}
